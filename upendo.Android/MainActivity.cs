f69b8c544bfeea6cfa588d5f1326884bf7383063
asuarezc <57622188+asuarezc@users.noreply.github.com>
2022-04-20 13:49:22 +0200

    Added droid project

---
+++ upendo.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using upendo.Managers;
+using upendo.Droid.Services;
+using upendo.CrossCutting.Interfaces.Data.LocalDB;
+using Xamarin.Essentials;
+
+namespace upendo.Droid
+{
+    [Activity(Label = "upendo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            RegisterNativeTypes();
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void RegisterNativeTypes()
+        {
+            DependencyServiceManager.Instance.Register<IConnectionStringProviderForLocalDB, ConnectionStringProviderForLocalDB>();
+        }
+    }
+}
---
+++ upendo.Android/Services/ConnectionStringProviderForLocalDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using upendo.CrossCutting.Interfaces.Data.LocalDB;
+using upendo.Helpers;
+
+namespace upendo.Droid.Services
+{
+    public class ConnectionStringProviderForLocalDB : IConnectionStringProviderForLocalDB
+    {
+        private static readonly string databaseName = "upendoLocalDatabase.db";
+
+        public string GetConnectionStringForLocalDB()
+        {
+            string path = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Personal), databaseName
+            );
+
+            //if (!File.Exists(path))
+            //    File.Create(path).Dispose();
+
+            string password = PasswordForLocalDBFactory.GetLocalDBPassword();
+
+            return $"Filename={path};Connection=direct;Password={password}";
+        }
+    }
+}
